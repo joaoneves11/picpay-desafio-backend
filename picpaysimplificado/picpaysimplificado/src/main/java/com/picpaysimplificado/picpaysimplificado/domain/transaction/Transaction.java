520375efcfd1855edfa5d5b81c978c91d6e8cdba
João Neves <joaopedro@outlook.com.br>
2024-02-21 20:51:59 -0300

    create transaction

---
+++ picpaysimplificado/picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User sender;
+
+    @ManyToOne
+    private User receiver;
+
+    public Transaction() {
+
+    }
+}
